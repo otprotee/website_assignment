bbee1e562d61c7c8d09f787aa242f15cc9ff280c
otprotee <oscarsmessages@gmail.com>
2025-02-12 15:27:17 +0000

    2

---
+++ scr/javascript/index.js
@@ -0,0 +1,10 @@
+
+function size(){
+	let sw = screen.width;
+	let sh = screen.height;
+	let ash = sh * 0.20;
+	console.log(ash);
+	console.log(sw + 'x' + sh);
+	document.getElementById("w").style.width = 'sw';
+	document.getElementById("h").style.height = 'ash';
+}
